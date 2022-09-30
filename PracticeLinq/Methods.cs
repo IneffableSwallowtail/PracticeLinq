2af0f410aba33fba25b2ad7e0f314bde1cf5d0c9
A.J. Miller <ajmiller1206@gmail.com>
2022-09-30 10:09:23 -0400

    first commit

---
+++ PracticeLinq/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLinq
+{
+    public static class Methods
+    {
+        public static void PrintEnumerable(IEnumerable<string> enumerable)
+        {
+            foreach (string item in enumerable)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using PracticeLinq;
+using System.Linq;
+
+string[] videoGames = new string[]
+{
+    "Little Big Planet",
+    "Final Fantasy VII",
+    "DOOM",
+    "Hyper Light Drifter",
+    "Shadow of the Colossus",
+    "Ico",
+    "Bendy and the Ink Machine",
+    "Mother",
+    "Cave Story",
+    "Hollow Knight"
+};
+
+IEnumerable<string> sorted = videoGames.OrderBy(game => game.Length);
+Methods.PrintEnumerable(sorted);
